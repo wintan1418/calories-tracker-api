15ecb2f98010a978b65a3c4c3c3ca74121d90e2b
oluwadare <wintan1418@gmail.com>
2021-09-22 10:15:06 +0100

    Implement create and show method to validate authenticity of user credentials'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+def create
+  @user = User.find_by(email:params[:email ])
+  return render json: {success: false, message: 'There is a user with this Email'}, status: 409 if @user
+
+  user =User.create!(user_params)
+  render json: user, status: :successful_creation
+end
+
+def show
+  @user = User.find(params[:email])
+  return render json: @user, status: :permit if @user
+end
+
+
+
+
+
 end
