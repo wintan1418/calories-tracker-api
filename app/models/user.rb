7b86d758b6d8b2415357bc89b23dbbb9fe34f9e7
oluwadare <wintan1418@gmail.com>
2021-09-22 13:15:51 +0100

    Add validation for user in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-  has_many :readings
+  self.primary_key = 'email'
+  has_many :readings, foreign_key: :user_email
+
+
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates :username, presence: true, uniqueness: {case_sensitive: false }
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 end
