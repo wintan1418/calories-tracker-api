ca9933f2a9fdf0a2a1f4570aed0dc925a85dd209
oluwadare <wintan1418@gmail.com>
2021-09-22 13:29:51 +0100

    Add authentication conditions in permit_request

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,3 +1,8 @@
 class AuthController < ApplicationController
 before_action :authorize_request
+
+def permit_request
+  AuthorizationServices.new(request.headers).authenticate_request!
+rescue JWT:: VerificationError, JWT::DecodeError  
+  render json: {errors: ['You are not Authenticating'], status: :unauthorized}
 end
