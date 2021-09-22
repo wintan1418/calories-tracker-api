aaa5c81f6e248444a4e7ea843ca8c7f76a4fa43f
oluwadare <wintan1418@gmail.com>
2021-09-22 09:59:36 +0100

    Add active record auth for controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord:: RecordNotFound, with: :not_found
+rescue_from ActiveRecord:: RecordNotUnique, with: :not_unique
+rescue_from ActiveRecord:: RecordInvalid, with: :invalid
+
+private
+
+def not_found(errors)
+  render json: errors,status: :not_found
+end
+
 end
