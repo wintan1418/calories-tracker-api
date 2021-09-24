e53d3abcc96a9a1726db41e29eeb53e2c655ad36
oluwadare <wintan1418@gmail.com>
2021-09-24 17:24:04 +0100

    Add validation for user params in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   validates :username, presence: true, uniqueness: {case_sensitive: false }
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :sex, presence: true
+  validates :age, numericality: {only_integer:true }
 end
