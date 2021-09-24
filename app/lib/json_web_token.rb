915332bc3facedb8962dd37c06917ab945bf3f43
oluwadare <wintan1418@gmail.com>
2021-09-24 16:54:42 +0100

    Refactor codes in json web toke file

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -2,31 +2,21 @@ require 'net/http'
 require 'uri'
 
 class JsonWebToken
-  def self.verify(token)
-    JWT.decode(token, nil,
-               true, # Verify the signature of this token
-               algorithm: 'RS256',
-               iss: ENV['AUTH0_DOMAIN'],
-               verify_iss: true,
-               aud: ENV['IDENTIFIER'],
-               verify_aud: true) do |header|
-      jwks_hash[header['kid']]
+  HMAC_SECRET = Rails.application.secrets.secret_key_base
+  
+    def self.encode(payload, exp = 24.hours.from_now)
+      # set timeout to 24 hours from time generated
+      payload[:exp] = exp.to_i
+      # 
+      JWT.encode(payload, HMAC_SECRET)
     end
-  end
-
-  def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("#{ENV['AUTH0_DOMAIN']}.well-known/jwks.json")
-    jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
-    Hash[
-      jwks_keys
-        .map do |k|
-        [
-          k['kid'],
-          OpenSSL::X509::Certificate.new(
-            Base64.decode64(k['x5c'].first)
-          ).public_key
-        ]
-      end
-    ]
-  end
+  
+    def self.decode(token)
+      # get payload;
+      body = JWT.decode(token, HMAC_SECRET)[0]
+      HashWithIndifferentAccess.new body
+      # grab all errors
+    rescue JWT::DecodeError => e
+      raise ExceptionHandler::InvalidToken, e.message
+    end
 end
