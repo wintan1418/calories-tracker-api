d87a67f7b6829eba9265620170569e763e052ec7
oluwadare <wintan1418@gmail.com>
2021-10-05 00:22:04 +0100

    Change authenticity  in controller

--- config/application.rb
+++ config/application.rb
@@ -22,17 +22,17 @@ Bundler.require(*Rails.groups)
 module CalorieTracker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: [:get, :patch, :put, :delete, :post, :options]
-          )
-      end
-    end
+    # config.load_defaults 6.1
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource(
+    #       '*',
+    #       headers: :any,
+    #       methods: [:get, :patch, :put, :delete, :post, :options]
+    #       )
+    #   end
+    # end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
