80413520c667133504f3f329ff20b1c9e252646d
oluwadare <wintan1418@gmail.com>
2021-10-04 20:02:22 +0100

    Fix params required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
-  
+  # protect_from_forgery with: :exception
+
   
   include Response
   include ExceptionHandler
--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,5 +1,5 @@
 class ReadingsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create]
+  # skip_before_action :verify_authenticity_token, only: [:create]
 
 
   
