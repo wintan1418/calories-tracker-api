c60b2f10a4228b11e75211fbb2b2fa72d59d033f
oluwadare <wintan1418@gmail.com>
2021-10-05 15:59:49 +0100

    Add test case for application controller

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+Rspec.describe ApplicationController, type: :controller do
+  let!(:user) { create(:user) }
+  # set headers for authorization
+  let(:headers) { { 'Authorization' => token_generator(user.id) } }
+  let(:invalid_headers) { { 'Authorization' => nil } }
+
+  describe '#authorize_request' do
+    context 'when  a auth token is  being passed' do
+      before { allow(request).to receive(:headers).and_return(headers) }
+
+      # private method authorize_request returns current user
+      it 'sets the current user' do
+        expect(subject.instance_eval { authorize_request }).to eq(user)
+      end
+    end
+
+    context 'when a auth token is not neing passed' do
+      before do
+        allow(request).to receive(:headers).and_return(invalid_headers)
+      end
+
+      it 'raises a MissingToken error' do
+        expect { subject.instance_eval { authorize_request } }
+          .to raise_error(ExceptionHandler::MissingToken, /Missing token/)
+      end
+    end
+  end
+end
