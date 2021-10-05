aff052b7bddf75482f5a12728ab8aaeb0b867032
oluwadare <wintan1418@gmail.com>
2021-10-05 14:23:25 +0100

    Update cors.rb and application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,20 +19,11 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CalorieTracker
+module IeltsPlannerApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 6.1
-    # config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins '*'
-    #     resource(
-    #       '*',
-    #       headers: :any,
-    #       methods: [:get, :patch, :put, :delete, :post, :options]
-    #       )
-    #   end
-    # end
+    config.load_defaults 6.1
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
