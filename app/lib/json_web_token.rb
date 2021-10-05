65c122822cb4ac896c76ddf9670ec6b64ca03b06
oluwadare <wintan1418@gmail.com>
2021-10-05 14:41:07 +0100

    Update cors.rb and application.rb

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,20 +1,21 @@
-
 class JsonWebToken
-  WINTAN = Rails.application.secrets.secret_key_base
-  
-    def self.encode(payload, exp = 24.hours.from_now)
-      # set timeout to 24 hours from time generated
-      payload[:exp] = exp.to_i
-      # 
-      JWT.encode(payload, WINTAN)
-    end
-  
-    def self.decode(token)
-      # get payload;
-      body = JWT.decode(token, WINTAN)[0]
-      HashWithIndifferentAccess.new body
-      # grab all errors
-    rescue JWT::DecodeError => e
-      raise ExceptionHandler::InvalidToken, e.message
-    end
+  # secret to encode and decode token
+  HMAC_SECRET = Rails.application.secret_key_base
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    # set expiry to 24 hours from creation time
+    payload[:exp] = exp.to_i
+    # sign token with application secret
+    JWT.encode(payload, HMAC_SECRET)
+  end
+
+  def self.decode(token)
+    # get payload; first index in decoded Array
+    body = JWT.decode(token, HMAC_SECRET)[0]
+    HashWithIndifferentAccess.new body
+    # rescue from all decode errors
+  rescue JWT::DecodeError => e
+    # raise custom error to be handled by custom handler
+    raise ExceptionHandler::InvalidToken, e.message
+  end
 end
