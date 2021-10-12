8e2d12cc60736b7f9c29eaee27c03aeb8c2308bf
oluwadare <wintan1418@gmail.com>
2021-10-12 19:31:05 +0100

    Fix rotes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'users#index'
   post 'auth/login', to: 'auth#authenticate'
   post '/register', to: 'users#create'
-  post 'details', to: 'users#info'
+  post 'details', to: 'users#details'
 
   resources :readings
   resources :meals, only: %i[index show]
